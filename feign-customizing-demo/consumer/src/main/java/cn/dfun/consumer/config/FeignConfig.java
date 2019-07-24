028c35b2262930ef98a51e554ac8040facb1f9e3
Vincent <wu_peizhen@126.com>
2019-07-24 23:52:50 +0800

    配置feign日志

--- feign-customizing-demo/consumer/src/main/java/cn/dfun/consumer/config/FeignConfig.java
+++ feign-customizing-demo/consumer/src/main/java/cn/dfun/consumer/config/FeignConfig.java
@@ -2,6 +2,7 @@ package cn.dfun.consumer.config;
 
 import cn.dfun.consumer.annotation.ExcludeFromComponentScan;
 import feign.Contract;
+import feign.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +12,10 @@ public class FeignConfig {
     @Bean
     public Contract feignContract() {
         return new Contract.Default();
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
