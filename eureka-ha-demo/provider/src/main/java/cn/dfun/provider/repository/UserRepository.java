501bcda9f2317139319407fcb9239b0a19f151bc
Vincent <wu_peizhen@126.com>
2019-07-25 01:16:25 +0800

    eureka ha

---
+++ eureka-ha-demo/provider/src/main/java/cn/dfun/provider/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package cn.dfun.provider.repository;
+
+import cn.dfun.provider.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
